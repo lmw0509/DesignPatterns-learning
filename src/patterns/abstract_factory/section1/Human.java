ded09883715660e6f00688c1d129372c9b318542
lmw <14705151653@163.com>
2019-02-12 23:00:19 +0800

    修改抽象工厂模式代码

---
+++ src/patterns/abstract_factory/section1/Human.java
@@ -0,0 +1,19 @@
+package patterns.abstract_factory.section1;
+
+/**
+ * @author cbf4Life cbf4life@126.com
+ * I'm glad to share my knowledge with you all.
+ * 定义一个人类的统称
+ */
+public interface Human {
+
+    //每个人种都有相应的颜色
+    void getColor();
+
+    //人类会说话
+    void talk();
+
+    //每个人都有性别
+    void getSex();
+
+}
